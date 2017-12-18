18132cda4d2b9e7e4723e1d853989b7db9787dcb
RyanWalt <waltr@mail.gvsu.edu>
2017-12-17 21:15:04 -0600

    Shell of the server.

--- TankServer.java
+++ TankServer.java
@@ -4,12 +4,47 @@ import java.nio.*;
 import java.nio.channels.*;
 
 public class TankServer{
-    public TankServer(int portNum){
+    int portNum;
+    public TankServer(int port){
+        portNum = port;
+        runServer();
+    }
 
+    public void runServer(){
+        try{
+            ServerSocketChannel channel = ServerSocketChannel.open();
+            channel.bind(new InetSocketAddress(portNum));
+            System.out.println("Server connected.");
+
+            while(true){
+                SocketChannel sockChan = channel.accept();
+
+                //Start a thread for each client (max 2)
+                Thread t = new Thread(new Runnable() {
+                    public void run() {
+                        //Code to run in thread
+                        runThread(sockChan);
+                    }
+                });
+                t.start();
+            }
+        }
+        catch(Exception e){
+            System.out.println("Error in runServer: " + e);
+        }
+    }
+
+    public void runThread(SocketChannel sc){
+        SocketChannel sockChan = sc;
+        System.out.println("Thread created.");
     }
 
 
-    public void main(String[] args){
+    public static void main(String[] args){
+        Console cons = System.console();
+        String portString = cons.readLine("Port num: ");
+        int portNum = Integer.parseInt(portString);
+        TankServer t = new TankServer(portNum);
 
     }
 }
